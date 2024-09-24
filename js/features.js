8f6a2776bf9d4e57d4d9083200244c5ead9c9432
tainur <tainurrahaman31@gmail.com>
2024-09-24 09:40:03 +0600

    made toggle feature between donation and history button

---
+++ js/features.js
@@ -0,0 +1,10 @@
+
+document.getElementById('donation-btn').addEventListener('click',function(event){
+    event.preventDefault;
+   togglebtn('donation-section');
+})
+
+document.getElementById('history-btn').addEventListener('click',function(event){
+    event.preventDefault;
+    togglebtn('history-section');
+})
---
+++ js/utilities.js
@@ -0,0 +1,7 @@
+
+function togglebtn(id){
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+
+    document.getElementById(id).classList.remove('hidden');
+}
