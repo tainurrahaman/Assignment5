bffe1c09eb35b03ec6ac5f53f3d39325dec12a73
tainur <tainurrahaman31@gmail.com>
2024-09-24 22:58:08 +0600

    seriously this is the final submission

--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
 
 document.getElementById('blog-btn').addEventListener('click',function(event){
     event.preventDefault;
-    window.location.href = './home.html'
+    window.location.href = './home.html';
 })
 
