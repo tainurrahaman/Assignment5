e7566b882ea7ac40b247f55ccc8a1eb9a07efd37
tainur <tainurrahaman31@gmail.com>
2024-09-24 23:06:31 +0600

    i have to no words

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 document.getElementById('home-btn').addEventListener('click',function(event){
     event.preventDefault;
-    window.location.href = './index.html'
+    window.location.href = './index.html';
 })
